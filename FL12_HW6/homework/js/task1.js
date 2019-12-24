892f8b5be0f7a0b42c7ae83b59fb542659e05617
Ihor <igorzdebskij@gmail.com>
2019-12-24 18:03:36 +0200

    noeror

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -2,25 +2,23 @@
 const a=prompt("Pleas enter a ",NaN);
 const b=prompt("Pleas enter b ",NaN);
 const c=prompt("Pleas enter c ",NaN);
-if(isNaN(a) || isNaN(b) || isNaN(c)==true)
- {
+const disk_B=4;
+if(isNaN(a) || isNaN(b) || isNaN(c)===true){
      console.log("Invalid input data");
- }
- else{
-    var diskriminant=b*b-(4*(a*c));
+}else{
+    var diskriminant=b*b-disk_B*a*c;
     console.log(diskriminant);
     if(diskriminant < 0){
-        console.log ("No Solution");
-    }
-    else if(diskriminant==0){
-        var x=(-b/2*a)
+    console.log("No Solution");
+    
+    }else if(diskriminant===0){
+        var x=-b/2*a;
         console.log("x="+x);
-   }
-    else{
-        var x1=(-(b)+Math.sqrt(diskriminant))/2;
-        var x2=(-(b)-Math.sqrt(diskriminant))/2;
+    }else{
+        var x1=-b+Math.sqrt(diskriminant)/2;
+        var x2=-b-Math.sqrt(diskriminant)/2;
         console.log("x1="+x1+"x2="+x2);
     }
     
 
- }
+}
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -2,31 +2,29 @@
 var a=prompt("Pleas enter a ","");
 var b=prompt("Pleas enter b ","");
 var c=prompt("Pleas enter c ","");
-if(isNaN(a) || isNaN(b) || isNaN(c)==true)
- {
+if(isNaN(a) || isNaN(b) || isNaN(c)===true){
      console.log("input values should be ONLY numbers ");
- }
- else if((a||b||c)=="" ){
+}else if((a||b||c)==="" ){
      console.log("input values should be ONLY numbers " );
- }
- else if(a<=0|| b<=0||c<=0) {
+ 
+}else if(a<=0|| b<=0||c<=0) {
      console.log("A triangle must have 3 sides with a positive definite length");
  }
  a=parseFloat(a);
  b=parseFloat(b);
  c=parseFloat(c);
- if((a+b>c)&&(b+c>a)&&(a+c>b)){
-     if(a==b==c){
+ if(a+b>c && b+c>a && a+c>b){
+     if(a===b===c){
          console.log("Equilateral triangle");
-     }
-     else if(a===b || b===c ||c===a){
+     
+     }else if(a===b || b===c ||c===a){
          console.log("Isosceles triangle");
-     }
-     else{
+     
+     }else{
          console.log("Scalene triangle")
      }
    
- }
- else{
+ 
+    }else{
      console.log("Triangle doesn’t exist");
  }
